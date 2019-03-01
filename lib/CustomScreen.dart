a6f2a84d35d0c8f5f9231eac0d8e434a3176dcbc
Matt Staley <matt@staleylegacy.com>
2019-03-01 02:07:14 -0500

    added hard coded list dropdownbutton to all four - Airport, Aircraft, Heading and Action

--- lib/CustomScreen.dart
+++ lib/CustomScreen.dart
@@ -18,7 +18,10 @@ class CustomScreenState extends State<CustomScreen>  {
   //String dropdownValue;
 
   // ignore: unused_field
-  String _selectedAirport = 'Airport Name;
+  String selectedAirport;
+  String selectedAction;
+  String selectedAircraft;
+  String selectedHeading;
 
 
   @override
@@ -26,7 +29,6 @@ class CustomScreenState extends State<CustomScreen>  {
     return new Scaffold(
       body: new Center(
         child: new Column(
-
           children: <Widget>[
 
             SizedBox(
@@ -40,27 +42,57 @@ class CustomScreenState extends State<CustomScreen>  {
               ),
               child: Container(
                 height: 60.0,
-                width: 200.0,
+                width: 325.0,
                 child: Row(
                   children: <Widget>[
-                    Expanded (
-                      child: new DropdownButton<String>(
+                     Center (
 
-                          items: <String>['50R LocakHart', '3T5 Fayette', '84R Smithville', 'T91 Carter', 'GYB Giddings', 'T20 Dreyer', 'HYI San Macros', '66R Wells', 'RYW Lago Vista', '1T8 Bulverde', '3R9 Lakeway', 'T82 Gillespie'].map((String value) {
+                      child: new DropdownButton<String>(
+                        onChanged: (String newVal) {
+
+                          setState(() {
+                            selectedAirport = newVal;
+
+                            print("Selected: ${selectedAirport}");
+                          });
+                        },
+                        value: selectedAirport,
+                        items: <String>['50R LocakHart', '3T5 Fayette', '84R Smithville', 'T91 Carter', 'GYB Giddings', 'T20 Dreyer', 'HYI San Macros', '66R Wells', 'RYW Lago Vista', '1T8 Bulverde', '3R9 Lakeway', 'T82 Gillespie'].map<DropdownMenuItem<String>>((String value) {
                             return new DropdownMenuItem<String>(
                               value: value,
-                              child: new Text(value),
+                              child: Text(value),
                             );
                           }).toList(),
-                          hint: Text("Airport Name"),
+                          hint: Text("Airport"),
 
-                          onChanged: (newVal) {
-                            _selectedAirport = newVal;
-                            this.setState(() {});
-                          },
+
+
+
                         ),
-
                     ),
+
+                      new DropdownButton<String>(
+                        onChanged: (String newVal) {
+
+                          setState(() {
+                            selectedHeading = newVal;
+                            print("Selected: ${selectedHeading}");
+                          });
+                        },
+                        value: selectedHeading,
+                        items: <String>['18', '36', '16', '35', '17', '35', '13', '31', '8', '26', '14', '32'].map<DropdownMenuItem<String>>((String value) {
+                          return new DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        hint: Text("Heading"),
+
+
+
+                      ),
+
+
                   ],
 
                 ),
@@ -81,14 +113,23 @@ class CustomScreenState extends State<CustomScreen>  {
                 child: Row(
                   children: <Widget>[
                     Expanded (
-                      child: new Text(
-                        'Aircraft:',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
+                      child: new DropdownButton<String>(
+                        onChanged: (String newVal) {
+
+                          setState(() {
+                            selectedAircraft = newVal;
+                            print("Selected: ${selectedAircraft}");
+                          });
+                        },
+                        value: selectedAircraft,
+                        items: <String>['Tomahawk 	9 1 3 7 2', 'Warrior 	4 7 4 2 6', 'Cessna 	2 0 7 8 Z', 'Cessna 	7 3 5 K K'].map<DropdownMenuItem<String>>((String value) {
+                          return new DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        hint: Text("Aircraft"),
+
                       ),
                     ),
                   ],
@@ -110,14 +151,23 @@ class CustomScreenState extends State<CustomScreen>  {
                 child: Row(
                   children: <Widget>[
                     Expanded (
-                      child: new Text(
-                        'Action:',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
+                      child: new DropdownButton<String>(
+                        onChanged: (String newVal) {
+
+                          setState(() {
+                            selectedAction = newVal;
+                            print("Selected: ${selectedAction}");
+                          });
+                        },
+                        value: selectedAction,
+                        items: <String>['Downwind', 'Base', 'Final'].map<DropdownMenuItem<String>>((String value) {
+                          return new DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        hint: Text("Action"),
+
                       ),
                     ),
                   ],
